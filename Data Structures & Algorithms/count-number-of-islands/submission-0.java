c2f83a8371d71ce31464159f2c91c02e0260b8af
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-08-13 23:31:37 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void helper(char[][] grid, int i,int j){
+        int n=grid.length;
+        int m=grid[0].length;
+
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0') return;
+        grid[i][j]='0';
+
+        helper(grid,i+1,j);
+        helper(grid,i-1,j);
+        helper(grid,i,j+1);
+        helper(grid,i,j-1);
+
+    }
+    public int numIslands(char[][] grid) {
+         int n=grid.length;
+        int m=grid[0].length;
+
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    helper(grid,i,j);
+                }
+            }
+        }
+
+
+        return count;
+    }
+}
